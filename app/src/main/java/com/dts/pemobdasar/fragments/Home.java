29d01d70eca0701ea06048f8c4421f3b6dcf676d
Fikrimusyari <fikricf123@gmail.com>
2025-04-04 01:23:54 +0700

    initial commit

---
+++ app/src/main/java/com/dts/pemobdasar/fragments/Home.java
@@ -0,0 +1,23 @@
+package com.dts.pemobdasar.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.dts.pemobdasar.R;
+
+public class Home extends Fragment {
+    View view;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+}
