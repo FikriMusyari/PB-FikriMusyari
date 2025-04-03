2ea1c9abb95c5708b47845305b9b3dfc315fdc88
Fikrimusyari <fikricf123@gmail.com>
2025-04-04 01:19:16 +0700

    initial commit

--- app/src/main/java/com/dts/pemobdasar/HomeActivity.java
+++ app/src/main/java/com/dts/pemobdasar/HomeActivity.java
@@ -2,23 +2,52 @@ package com.dts.pemobdasar;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.dts.pemobdasar.fragments.Profile;
+import com.dts.pemobdasar.fragments.Setting;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.dts.pemobdasar.fragments.Home;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setSelectedItemId(R.id.nav_home);
+        bottomNav.setOnItemSelectedListener(navListener);
+
+        Fragment selectedFragment = new Home();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                selectedFragment).commit();
+
     }
+
+    private NavigationBarView.OnItemSelectedListener navListener =
+            item -> {
+                int itemId = item.getItemId(); /* obtain the selected item ID from your source */
+                Fragment selectedFragment = null;
+
+                if (itemId == R.id.nav_profile) {
+                    selectedFragment = new Profile();
+                } else if (itemId == R.id.nav_home) {
+                    selectedFragment = new Home();
+                } else if (itemId == R.id.nav_settings) {
+                    // Handle the profile case
+                    selectedFragment = new Setting();
+
+                } else {
+                    selectedFragment = new Home();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            };
+
 }
